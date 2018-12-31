bc8d501d250835fb8ce6308881a7d1648fb8b80d
Marco Dalla Stella <marco@7bridges.eu>
2018-12-31 12:22:32 +0100

    Add a minimal `-main` function

--- src/vinnik/core.clj
+++ src/vinnik/core.clj
@@ -1,1 +1,12 @@
-(ns vinnik.core)
+(ns vinnik.core
+  (:require [vinnik.board :as board]))
+
+(def initial-state
+  {:board board/initial-board
+   :player :white
+   :turn 1})
+
+(defn -main []
+  (-> initial-state
+      :board
+      board/pprint))
